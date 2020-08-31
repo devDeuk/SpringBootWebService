8820ef014021a450a26824b582e52eed94e8a0cb
devdeuk <ksd83@naver.com>
2020-08-31 11:49:10 +0900

    2020.08.31

--- src/main/java/com/devdeuk/springboot/service/posts/PostsService.java
+++ src/main/java/com/devdeuk/springboot/service/posts/PostsService.java
@@ -27,6 +27,7 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
--- src/main/java/com/devdeuk/springboot/web/PostApiController.java
+++ src/main/java/com/devdeuk/springboot/web/PostApiController.java
@@ -22,6 +22,8 @@ public class PostApiController {
         return postsService.save(requestDto);
     }
 
+
+
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
         return postsService.update(id, requestDto);
